e51d99aa3fa54b1b8016ac9d9daed204ec36431e
cheney <lamost710@126.com>
2014-03-14 10:56:32 +0800

    save

---
+++ AppPlatform/AppPlatform.UI/Controllers/AuthenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppPlatform.UI.Controllers
+{
+    public class AuthenController : Controller
+    {
+        //
+        // GET: /Authen/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
